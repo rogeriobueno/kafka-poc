935b37a756f68fa870ae7cede1ca608235766ad2
Rogerio Bueno <bueno.rogerio@gmail.com>
2020-01-27 18:09:38 -0300

    Adicionado projeto DEMO

---
+++ kafka.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/bueno/workspace/kafka_2.12-2.3.0/bin/kafka-server-start.sh /home/bueno/workspace/kafka_2.12-2.3.0/config/server.properties
---
+++ zookeeper.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/bueno/workspace/kafka_2.12-2.3.0/bin/zookeeper-server-start.sh /home/bueno/workspace/kafka_2.12-2.3.0/config/zookeeper.properties
